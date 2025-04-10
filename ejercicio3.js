4613b2ca96bcc010e33ce7aea1b8676f00fd00ff
JonasValCas <j8028587@gmail.com>
2025-04-10 18:03:35 -0500

    Agregar lodash y usar en ejercicio 3

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,3 +1,5 @@
-let array = [1, 2, 3, 4, 5, 25];
-let sumaTotal = array.reduce((acc, curr) => acc + curr, 0);
+const _ = require('lodash');  // Importamos lodash
+
+let array = [1, 2, 3, 4, 5];
+let sumaTotal = _.sum(array);  // Usamos _.sum() de lodash
 console.log(`La suma total del array es: ${sumaTotal}`);
